1e87babdfd5e2ffc7f9110cc5262fe3d2a1f79df
taymas <taymasnguyen@gmail.com>
2025-09-16 11:24:45 -0700

    granulate recent data

--- yahoo.py
+++ yahoo.py
@@ -8,6 +8,13 @@ def get_info(ticker_name):
     # get ticker information
     # yahoo only downloads last 7 days of data if interval is 1 minute
     # TODO get max interval data and replace last 7 days with 1 minute interval data
+    # how to complete TODO ^^^ : make list - append all max interval data to list - stop when time is less than the first of the 1 minute interval data
+    # ------ psuedocode -------
+    # for i in max_data:
+    #     if max_data.date < min_data_first_date:
+    #         new_data.append(i)
+    #     else:
+    #         new_data.append(min_data)
     minute_hist = yf.download(ticker_name, period="max", interval="1m")
     max_hist = yf.download(ticker_name, period="max", interval="1d")
     original_timezone = "UTC"
@@ -31,12 +38,19 @@ def get_info(ticker_name):
     # cut off time zone from format
     max_dates = list(tz.timezone(original_timezone).localize(datetime.strptime((str(i)), "%Y-%m-%d %H:%M:%S")).astimezone(tz.timezone(target_timezone)).strftime("%Y-%m-%d %H:%M:%S") for i in list(max_hist.index))
     minute_dates = list(tz.timezone(original_timezone).localize(datetime.strptime((str(i)[:-6]), "%Y-%m-%d %H:%M:%S")).astimezone(tz.timezone(target_timezone)).strftime("%Y-%m-%d %H:%M:%S") for i in list(minute_hist.index))
-    print(max_dates[10:])
-    print(minute_dates[100:])
+    target_timezone
+
     # output data into list of strings into csv format
     data = [['date', 'value']]
+
+    # add data from max interval data except last 7 days
     for i,j in zip(max_dates,max_close):
+        if tz.timezone(target_timezone).localize(datetime.strptime(i, "%Y-%m-%d %H:%M:%S")) < result_date: 
+            data.append([i,j])
+
+    # add data from all of minute interval
+    for i,j in zip(minute_dates,minute_close):
         data.append([i,j])
- 
+        
     return data 
 

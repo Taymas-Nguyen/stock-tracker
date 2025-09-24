0ba1ae2a4db4dd9cdf8099a635ab7919a70b3480
taymas <taymasnguyen@gmail.com>
2025-09-23 21:02:01 -0700

    add whitenoise

---
+++ staticfiles/data_page.js
@@ -0,0 +1,48 @@
+// on page load, graph default max range from visualize.js
+document.addEventListener('DOMContentLoaded', function() {
+    drawLine("steelblue", "Max");
+    document.getElementById('loading_graph').style.display = 'none';
+});
+
+// array of all of range buttons
+var childDivs = document.getElementById('range_buttons').getElementsByTagName('button');
+
+// go through array and remove background, highlight max
+for( i=0; i< childDivs.length; i++ )
+{
+    if (childDivs[i].id == "Max"){
+        childDivs[i].style.backgroundColor = 'yellow';
+    }
+    else{
+        childDivs[i].style.backgroundColor = 'transparent';
+    }
+}
+
+function getRange(element, ticker_name) {
+    for( i=0; i< childDivs.length; i++ )
+    {
+        childDivs[i].disabled = true;
+    }
+    document.getElementById('loading_graph').style.display = 'inline';
+    removeSVG();
+    $.ajax({
+        type: "GET",
+        success: function() {
+            drawLine('red', element.id);
+            for( i=0; i< childDivs.length; i++ )
+            {
+                if (childDivs[i].id == element.id){
+                    childDivs[i].style.backgroundColor = 'yellow';
+                }
+                else{
+                    childDivs[i].style.backgroundColor = 'transparent';
+                }
+            }
+            document.getElementById('loading_graph').style.display = 'none';
+            for( i=0; i< childDivs.length; i++ )
+            {
+                childDivs[i].disabled = false;
+            }
+        },
+    });
+}

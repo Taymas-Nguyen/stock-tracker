e5c86ebd46cb1a3d1b494744f60e210e6f8036e7
taymas <taymasnguyen@gmail.com>
2025-10-22 15:31:50 -0700

    add getTopDiv()

--- src/static/data_page.js
+++ src/static/data_page.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var starting = 'stock0';
     topDiv = document.getElementById(starting);
+    topDiv.querySelector(`#${starting}-addGraph`).style.display = 'none';
+    topDiv.querySelector(`#${starting}-deleteGraph`).style.display = 'inline';
     range_buttons = topDiv.querySelectorAll('button');
     for( i=0; i< range_buttons.length; i++ )
         {
@@ -22,15 +24,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // on ANY range button press, get top level parent (i.e stock0, stock1, ect), modify only that div
 function rangeClicked(element, ticker_name) {
-    let topLevelDiv = element;
-    while (topLevelDiv.parentNode && topLevelDiv.parentNode !== document.body && topLevelDiv.parentNode !== document.documentElement) {
-        if (topLevelDiv.parentNode.tagName.toLowerCase() === 'div') {
-            topLevelDiv = topLevelDiv.parentNode;
-        }   
-    }
+    topDivName = getTopDiv(element);
+    
     rangeType = element.id.split("-")[1];
-    topDivName = topLevelDiv.id;
-
     removeSVG(topDivName);
     topDiv = document.getElementById(topDivName)
     range_buttons = topDiv.querySelectorAll('button');
@@ -54,23 +50,21 @@ function rangeClicked(element, ticker_name) {
 // hide only the graph and buttons, not the search bar. hide the delete icon and show the add icon
 // all graphs underneath should move up, only modify the top div 
 function deleteGraph(element){
-    let topLevelDiv = element;
-    while (topLevelDiv.parentNode && topLevelDiv.parentNode !== document.body && topLevelDiv.parentNode !== document.documentElement) {
-        if (topLevelDiv.parentNode.tagName.toLowerCase() === 'div') {
-            topLevelDiv = topLevelDiv.parentNode;
-        }   
-    }
-    topDivName = topLevelDiv.id;
+    topDivName = getTopDiv(element);
 }
 
 // hide the add icon and show the delete icon
 // all graphs/buttons reappear, only modify the top div, all graphs below should move down 
 function addGraph(element){
+    topDivName = getTopDiv(element);
+}
+
+function getTopDiv(element){
     let topLevelDiv = element;
     while (topLevelDiv.parentNode && topLevelDiv.parentNode !== document.body && topLevelDiv.parentNode !== document.documentElement) {
         if (topLevelDiv.parentNode.tagName.toLowerCase() === 'div') {
             topLevelDiv = topLevelDiv.parentNode;
         }   
     }
-    topDivName = topLevelDiv.id;
+    return topLevelDiv.id;
 }

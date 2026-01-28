d36650accd92908dca60f209f922d7b784a5d769
taymas <taymasnguyen@gmail.com>
2026-01-27 23:37:23 -0800

    disable debug and collectstatic

---
+++ src/staticfiles/data_page.js
@@ -0,0 +1,142 @@
+// varible accessible by js and python (via regex parse) to keep track of # of forms across entire app, index at 0
+amount_of_forms = 10;
+
+all_graphs = []
+for( i=0; i < amount_of_forms; i++ )
+{
+    all_graphs.push(`stock${i}`)
+}
+
+// visibility statuses for cache
+show = 'show' // show everything and the hide button
+invisible = 'invisible' // show nothing
+partial_show = 'partial_show' // show only filled out input field with the show button
+partial_invisible = 'partial_invisible' // show only empty input field with no buttons
+
+// on page load
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Iterate through the NodeList and remove the list tags that django adds
+    const elementsWithClass = document.querySelectorAll('.errorlist');
+    elementsWithClass.forEach(element => {
+        element.remove();
+    });
+    
+    // Iterate through all graphs
+    for (const stock of all_graphs) {
+
+        // if no visibility then default is invisible, else preserve visibility
+        get_visibility(stock).then(data =>{
+            if (data == null){
+                change_visibility(stock, invisible);
+            }
+        });
+
+        get_visibility(stock).then(data =>{
+            if (data == invisible){
+                document.querySelector(`#${stock}`).style.display = 'none';
+
+            }else if (data == partial_invisible){
+                // hide all range buttons
+                var range_buttons = document.querySelector(`#${stock}`).querySelectorAll('button');
+                for( i=0; i< range_buttons.length; i++ )
+                {
+                    range_buttons[i].style.display = 'none';
+                }
+                document.querySelector(`#${stock}`).style.display = 'inline';
+                document.querySelector(`#${stock}-line_graph`).style.display = 'none';
+                document.querySelector(`#${stock}-hideGraph`).style.display = 'none';
+            }else{
+                get_range(stock).then(data => {
+                    // enable all buttons again
+                    var range_buttons = document.querySelector(`#${stock}`).querySelectorAll('button');
+                    for( i=0; i< range_buttons.length; i++ )
+                    {
+                        range_buttons[i].disabled = false;
+                    }
+                    topDiv = document.getElementById(stock);
+                    rangeType = data == null ? "Max" : data;
+        
+                    loadingGraph(topDiv, stock, rangeType)
+                    drawLine('red', rangeType, stock);
+                });
+            }
+        });
+    }
+
+
+});
+
+// on ANY range button press, get top level parent (i.e stock0, stock1, ect), modify only that div
+function rangeClicked(element, ticker_name) {
+    topDivName = getTopDiv(element);
+
+    topDiv = document.getElementById(topDivName)
+    removeSVG(topDivName);
+
+    rangeType = element.id.split("-")[1];
+    loadingGraph(topDiv, topDivName, rangeType)
+
+    change_range(topDivName, rangeType);
+    drawLine('red', rangeType, topDivName);
+}
+
+// hides entire div except the input field, all graphs beneath should move up, replace hide button with show button
+function hideGraph(element){
+    topDivName = getTopDiv(element);
+}
+
+// reveals entire div, replace show button with hide button
+function showGraph(element){
+
+}
+
+// add new graph at the bottom, only show the input field, hide hide/show button
+function addGraph(element){
+    for (const stock of all_graphs){
+        if (document.querySelector(`#${stock}`).style.display == 'none'){
+            
+            // hide all range buttons
+            var range_buttons = document.querySelector(`#${stock}`).querySelectorAll('button');
+            for( i=0; i< range_buttons.length; i++ )
+            {
+                range_buttons[i].style.display = 'none';
+            }
+            
+            change_visibility(stock, partial_invisible);
+            document.querySelector(`#${stock}`).style.display = 'inline';
+            document.querySelector(`#${stock}-line_graph`).style.display = 'none';
+            document.querySelector(`#${stock}-hideGraph`).style.display = 'none';
+            return;
+        }
+    }
+}
+
+function getTopDiv(element){
+    let topLevelDiv = element;
+    while (topLevelDiv.parentNode && topLevelDiv.parentNode !== document.body && topLevelDiv.parentNode !== document.documentElement) {
+        if (topLevelDiv.parentNode.tagName.toLowerCase() === 'div') {
+            topLevelDiv = topLevelDiv.parentNode;
+        }   
+    }
+    return topLevelDiv.id;
+}
+
+// before graphs can load, disable all buttons, show the loading icon, and highlight the chosen range
+function loadingGraph(topDiv, topDivName, rangeType){
+    range_buttons = topDiv.querySelectorAll('button');
+    for( i=0; i< range_buttons.length; i++ )
+        {
+            range_buttons[i].disabled = true;
+        }
+    topDiv.querySelector(`#${topDivName}-loading_graph`).style.display = 'inline';
+    for( i=0; i< range_buttons.length; i++ )
+    {
+        if (range_buttons[i].id == `${topDivName}-${rangeType}`){
+            range_buttons[i].style.backgroundColor = 'yellow';
+        }
+        else{
+            range_buttons[i].style.backgroundColor = 'transparent';
+        }
+    }
+}
---
+++ src/staticfiles/visualize.js
@@ -0,0 +1,234 @@
+function drawLine(color, range, topDivName){
+  var csv_page;
+  var time_format;
+  var range_tooltip_format;
+  var past_date = new Date();
+  
+  // 5 days shows 7 days, this is intended
+  if (range == "1Day"){
+    csv_page = "minute";
+    past_date.setDate(past_date.getDate() - 1)
+    time_format = "%b %d %H:%M";
+    range_tooltip_format = {
+      month: 'short',
+      day: '2-digit',
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true
+    }
+  }
+  if (range == "5Days"){
+    csv_page = "minute";
+    past_date.setDate(past_date.getDate() - 7)
+    time_format = "%b %d %H:%M";
+    range_tooltip_format = {
+      month: 'short',
+      day: '2-digit',
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true
+    }
+  }
+  if (range == "1Month"){
+    csv_page = "max";
+    past_date.setDate(past_date.getDate() - 31)
+    time_format = "%b %d";
+    range_tooltip_format = {
+      month: 'short',
+      day: '2-digit',
+    }
+  }
+  if (range == "6Months"){
+    csv_page = "max";
+    past_date.setDate(past_date.getDate() - 185)
+    time_format = "%Y %b %d";
+    range_tooltip_format = {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+    }
+  }
+  if (range == "YearToDate"){
+    csv_page = "max";
+    past_date = new Date(new Date().getFullYear(), 0, 1);
+    time_format = "%Y %b";
+    range_tooltip_format = {
+      month: 'short',
+      day: '2-digit',
+    }
+  }
+  if (range == "1Year"){
+    csv_page = "max";
+    past_date.setDate(past_date.getDate() - 366)
+    time_format = "%Y %b";
+    range_tooltip_format = {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+    }
+  }
+  if (range == "5Years"){
+    csv_page = "max";
+    past_date.setDate(past_date.getDate() - 366*5)
+    time_format = "%Y %b";
+    range_tooltip_format = {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+    }
+  }
+  if (range == "Max"){
+    csv_page = "max";
+    time_format = "%Y %b";
+    range_tooltip_format = {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+    }
+  }
+
+  // set the dimensions and margins of the graph
+  var scale = 1;
+  const margin = {top: 10, right: 30, bottom: 30, left: 60};
+  const width = 1150/scale
+  const height = 400/scale
+
+  // append the svg object to line graph of top div with unique id
+  const svg = d3
+    .select(`#${topDivName}-line_graph`)
+    .append("svg")
+    .attr("id", `${topDivName}-svgid`)
+    .attr("style", "position: relative; right: 0px;")
+    .attr("viewBox", "0 0 1180 475")
+    // .attr("preserveAspectRatio", "none")
+    // .attr("width", width)
+    // .attr("height", height)
+    .attr("height", "100%")
+    // .attr("width", "100%")
+    // .attr("width", "auto")
+
+
+  
+
+    .append("g")
+    .attr("id", `${topDivName}-g`)
+    .attr("transform", `translate(35,55)`);  
+    
+
+
+  //Read the data
+  d3.csv(csv_page + topDivName[topDivName.length - 1], 
+
+  function(d){
+    // todo: change time format based on ranges
+    data_date = new Date(d.date)
+    if (data_date > past_date || range == "Max"){
+      return { date : d3.timeParse("%Y-%m-%d %H:%M:%S")(d.date), value : d.value }
+    }
+  }).then(
+    function(data){
+      // Add X axis --> it is a date format
+      const x = d3 
+        .scaleTime()
+        .domain(d3.extent(data, function(d) { 
+          return d.date; }))
+        .range([ 0, width ]);
+
+      svg
+        .append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x).tickFormat(d3.timeFormat(time_format))); // time format 
+
+      // Add Y axis
+      const y = d3 
+        .scaleLinear()
+        .domain([d3.min(data, function(d) { return +d.value; }), d3.max(data, function(d) { return +d.value; })])
+        .range([ height, 0 ]);
+      svg
+        .append("g")
+        .call(d3.axisLeft(y));
+
+      // Add the line
+      svg
+        .append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", color)
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) }))
+
+      // start of tooltip
+      const circle = svg
+        .append("circle")
+        .attr("r", 0)
+        .attr("fill", "steelblue")
+        .style("stroke", "white")
+        .attr("opacity", .70)
+        .style("pointer-events", "none");
+
+      const listeningRect = svg
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height);
+
+      const tooltip = d3
+        .select(`#${topDivName}-tooltip`)
+        .append("div")
+        .attr("class", "tooltip");
+
+      listeningRect.on("mousemove", function (event){
+          const [xCoord] = d3.pointer(event, this);
+          const bisectDate = d3.bisector(d => d.date).left;
+          const x0 = x.invert(xCoord);
+          const i = bisectDate(data, x0, 1);
+          const d0 = data[i - 1];
+          const d1 = data[i];
+          const d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+          const xPos = x(d.date);
+          const yPos = y(d.value);
+          
+          circle
+            .transition()
+            .duration(50)
+            .attr("r", 5)
+            .attr("cx", xPos)
+            .attr("cy", yPos);
+
+          const formatter = new Intl.DateTimeFormat('en-US', range_tooltip_format);
+
+          tooltip
+            .style("display", "block")
+            .style("left", xPos + 1)
+            .style("top", yPos + 1)
+            .html(`${formatter.format(d.date)} <br> ${d.value}`);
+        }
+      );
+
+      listeningRect.on("mouseleave", function () {
+          circle
+            .transition()
+            .duration(50)
+            .attr("r", 0);
+          tooltip
+            .style("display", "none");
+        }
+      );
+      // end of tooltip
+
+      // when svg is done rendering, enable buttons and hide loading screen
+      topDiv = document.getElementById(topDivName)
+      range_buttons = topDiv.querySelectorAll('button');
+      for( i=0; i< range_buttons.length; i++ )
+      {
+        range_buttons[i].disabled = false;
+      }
+      topDiv.querySelector(`#${topDivName}-loading_graph`).style.display = 'none';
+    }
+  )
+}
+
+function removeSVG(topDivName){
+  d3.select(`#${topDivName}-svgid`).remove();
+}
